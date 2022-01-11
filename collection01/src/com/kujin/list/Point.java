b5bd959ee8ce76e56590ce8bede4930196636342
leekykin <leekujin14@gmail.com>
2022-01-11 22:34:15 +0900

    2022-01-11

---
+++ collection01/src/com/kujin/list/Point.java
@@ -0,0 +1,41 @@
+package com.kujin.list;
+
+import java.util.*;
+
+public class Point {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void showPointUI() {
+        System.out.println("              ***성적표***");
+        System.out.println("===============================================");
+        System.out.println("학번   이름   국어   영어   수학   총점   평균   학점");
+        System.out.println("------------------------------------------------");
+
+    }
+
+
+    public static void inputPoints(List<Student> students) {
+
+    }
+
+    public static void showAllPoints(List<Student> students) {
+
+    }
+    public static void searchPoints(List<Student> students) {
+
+    }
+    public static void deletePoints(List<Student> students) {
+
+    }
+
+    public static void modifyPooints(List<Student> student) {
+
+    }
+
+    public static void menuInfo() {
+
+    }
+
+
+}
