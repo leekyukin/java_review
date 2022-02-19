853c3de25063d2ceccd7f80c7275e9f75413fe3d
leekyukin <leekujin14@gmail.com>
2022-02-19 15:34:42 +0900

    2022-02-19 Review How to use lamda expression

---
+++ lambdaExpression/src/com/kyukin/functions/Calc.java
@@ -0,0 +1,12 @@
+package com.kyukin.functions;
+
+public interface Calc {
+
+    public int add(int x, int y);
+
+//    public int subtract(int x, int y);
+//
+//    public int division(int x, int y);
+//
+//    public int multiply(int x, int y);
+}
---
+++ lambdaExpression/src/com/kyukin/functions/test01.java
@@ -0,0 +1,12 @@
+package com.kyukin.functions;
+
+public class test01 {
+
+    public static void main(String[] args) {
+
+        Calc add = (x, y) -> x + y;
+
+        System.out.println(add.add(1,2));
+
+    }
+}
